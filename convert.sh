decc6ff1783e6b98ccbf29144481eb9cd5d969cc
neocanable <neocanable@gmail.com>
2018-12-08 13:15:10 +0800

    complete compress image script

--- convert.sh
+++ convert.sh
@@ -1,13 +1,80 @@
 #!/bin/bash
+# change image file's md5
 
-# change png file's md5
-files=$(find ./ | grep png | grep -v '.bundle')
-for i in $files
-do
-  echo $i;
-  if [ ! -d "tmp" ]; then
-    mkdir tmp
-  fi
-  convert $i -quality 99 tmp/tmp.png
-  mv tmp/tmp.png $i
+
+usage() { 
+	echo "Usage: "
+	echo "  $0 [-d <iOS project path>] [-t <png|jpg|all>] [-q <compress image quality>]"; 
+	echo "  -d iOS project path | required";
+	echo "  -t type of images to compress, default is png";
+	echo "  -q compress quality, default is 99";
+}
+
+prepare_working_dir() {
+	if [[ ! -d compress_working_dir ]]; then
+		mkdir compress_working_dir
+	fi
+}
+
+clear_working_dir() {
+	if [[ -d ./compress_working_dir ]]; then
+		rm -rf ./compress_working_dir
+	fi
+}
+
+convert_images() {
+	project_dir=$1
+	image_type=$2
+	quality=$3
+	
+	if [ -z "${project_dir}" ]; then
+		usage;
+		exit 0;
+	fi
+
+	if [[ -z "${image_type}" ]]; then
+		image_type="png"
+	fi
+
+	if [[ -z "${quality}" ]]; then
+		quality="99"
+	fi	
+	
+	echo "project_dir:  ${project_dir}"
+	echo "image_type :  ${image_type}"
+	echo "quality    :  ${quality}"
+	
+	files=$(find $project_dir -type f -iregex ".*$image_type\$" | grep -v .git)
+	for file in $files
+	do
+		compressed_file=$(echo $file | awk -F '/' '{print $(NF-1)"_"$NF}')
+		if [[ ! -d "conver_image_tmp" ]]; then
+			mkdir conver_image_tmp
+		fi
+		echo "convert $file -quality $quality conver_image_tmp/$compressed_file"
+		convert $file -quality $quality conver_image_tmp/$compressed_file
+		mv conver_image_tmp/$compressed_file $file
+		# echo "i will mv $file to conver_image_tmp/$compressed_file"
+	done
+}
+
+while getopts ":d:t:q:" o; do
+    case "${o}" in
+        d)
+            project_dir=${OPTARG}
+            ;;
+        t)
+            image_type=${OPTARG}
+            ;;
+		q)
+			quality=${OPTARG}
+			;;
+        *)
+            # usage
+            ;;
+    esac
 done
+
+prepare_working_dir
+convert_images $project_dir $image_type $quality
+
